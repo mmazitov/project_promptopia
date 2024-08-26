c51fff0b8baee8a444b8c156c1f04bad59f1215b
mmazitov <mazitov.nikolya@gmail.com>
2024-08-26 15:38:43 +0300

    bd fix

--- utils/database.js
+++ utils/database.js
@@ -3,24 +3,22 @@ import mongoose from 'mongoose';
 let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-  mongoose.set('strictQuery', true);
+	mongoose.set('strictQuery', true);
 
-  if(isConnected) {
-    console.log('MongoDB is already connected');
-    return;
-  }
+	if (isConnected) {
+		console.log('MongoDB is already connected');
+		return;
+	}
 
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "share_prompt",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+	try {
+		await mongoose.connect(process.env.MONGODB_URI, {
+			dbName: 'share_prompt',
+		});
 
-    isConnected = true;
+		isConnected = true;
 
-    console.log('MongoDB connected')
-  } catch (error) {
-    console.error('Failed to connect to MongoDB:', error); 
-  }
-}
+		console.log('MongoDB connected');
+	} catch (error) {
+		console.error('Failed to connect to MongoDB:', error);
+	}
+};
